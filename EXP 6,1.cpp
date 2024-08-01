23f74e9da3ccd64812d64cc76fc68800caea1ee2
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-02 05:11:21 +0530

    Add files via upload

---
+++ EXP 6,1.cpp
@@ -0,0 +1,14 @@
+//RIDDHI LOKHANDE 
+//EXP 6.1
+//23070123107
+//ENTC B2
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for (i=0;i<10;i++) {
+    cout<<"HELLO WORLD("<<i+1<<")"<<"\n";
+    }
+    return 0;
+}
