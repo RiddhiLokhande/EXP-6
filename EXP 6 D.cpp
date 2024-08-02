a486e3a3df126f3d8c65511573ae2d85ca8c79df
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-02 09:37:18 +0530

    Add files via upload

---
+++ EXP 6 D.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    int matrix1[2][2] = {{1, 2}, {3, 4}};
+    int matrix2[2][2] = {{5, 6}, {7, 8}};
+    int result[2][2] = {0};  
+
+    
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            for (int k = 0; k < 2; ++k) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    std::cout << "Result matrix:" << std::endl;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            std::cout << result[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
