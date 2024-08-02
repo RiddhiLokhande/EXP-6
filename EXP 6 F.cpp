33e66cbad9b2e9638d5608693ad03d5723aa2016
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-02 09:59:54 +0530

    Add files via upload

---
+++ EXP 6 F.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main() {
+    int i = 1; 
+    do {
+        int j = 1;  
+        do {
+            std::cout << i * j << "\t";  
+            ++j;  
+        } while (j <= 10);
+
+        std::cout << std::endl;  
+        ++i;  
+    } while (i <= 10);
+
+    return 0;
+}
